b66572ae34a58394bf38f6f8189f294831222feb
Aldvor <=>
2024-08-19 01:21:21 +0300

    tests

--- src/components/protected-route/ProtectedRoute.tsx
+++ src/components/protected-route/ProtectedRoute.tsx
@@ -1,28 +1,33 @@
-import { Outlet, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { useSelector } from '../../services/store';
 import { Navigate } from 'react-router';
-import { TProtectedRouteProps } from './type';
-import { Login } from '@pages';
+import { Preloader } from '../ui/preloader';
+
+type ProtectedRouteProps = {
+  onlyUnAuth?: boolean;
+  children: React.ReactElement;
+};
 
 export const ProtectedRoute = ({
   children,
   onlyUnAuth = false
-}: TProtectedRouteProps) => {
-  const isAuthChecked = useSelector((state) => state.userData.isAuthChecked);
-  const user = useSelector((state) => state.userData.user);
+}: ProtectedRouteProps) => {
+  const isAuthChecked = useSelector((store) => store.userData.isAuthChecked);
+  const user = useSelector((store) => store.userData.user);
   const location = useLocation();
 
   if (!isAuthChecked) {
-    return <Login />;
+    return <Preloader />;
+  }
+
+  if (onlyUnAuth && user) {
+    const { from } = location.state || { from: { pathname: '/' } };
+    return <Navigate to={from} />;
   }
 
   if (!onlyUnAuth && !user) {
     return <Navigate to='/login' state={{ from: location }} />;
   }
 
-  if (onlyUnAuth && user) {
-    return <Navigate to='/' />;
-  }
-
-  return children ? children : <Outlet />;
+  return children;
 };
--- src/services/slices/userSlice.ts
+++ src/services/slices/userSlice.ts
@@ -52,7 +52,7 @@ const UserSlice = createSlice({
       state.user = action.payload;
     },
     authCheck: (state) => {
-      state.isAuthChecked = false;
+      state.isAuthChecked = true;
     }
   },
   selectors: {
